d4eb46a87fb629a787249865d41b18ee17c64f3b
luksfay <luksfaydev@gmail.com>
2022-10-20 03:50:42 -0300

    dinamic buttons

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,30 +1,159 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Categories.css";
+import CategoriesObject from "./CategoriesObject";
 const Categories = () => {
+  const [data, setData] = useState(CategoriesObject);
+
+  const filterResult = (cardItem) => {
+    const result = CategoriesObject.filter((item) => {
+      return item.category === cardItem;
+    });
+    setData(result);
+  };
   return (
-    <div>
-      <h2 className="title">Categorías</h2>
-      <div className="categories-container">
-        <button>Cargadores y Arrancadores</button>
-        <button className="categories-div">Chaperia</button>
-        <button className="categories-div">Compresores de Aire</button>
-        <button className="categories-div">Elevadores de Autos</button>
-        <button className="categories-div">Extractores y Comprimidos</button>
-        <button className="categories-div">Gomería</button>
-        <button className="categories-div">Herramientas Eléctricas</button>
-        <button className="categories-div">Herramientas Especiales</button>
-        <button className="categories-div">Herramientas Manuales</button>
-        <button className="categories-div">Insertos</button>
-        <button className="categories-div">Inyección Electrónica</button>
-        <button className="categories-div">Juegos de Tubos</button>
-        <button className="categories-div">Lubricación</button>
-        <button className="categories-div">Mobiliario para Talleres</button>
-        <button className="categories-div">Prensas e Hidraulicos</button>
-        <button className="categories-div">Puesta a Punto</button>
-        <button className="categories-div">Sacabollos</button>
-        <button className="categories-div">Soldadoras</button>
+    <>
+      <div>
+        <h2 className="title">Categorías</h2>
+        <div className="categories-container">
+          <button
+            className="categories-div"
+            onClick={() => setData(CategoriesObject)}
+          >
+            Todos
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("cargadoresyarrancadores")}
+          >
+            Cargadores y Arrancadores
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("chaperia")}
+          >
+            Chaperia
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("compresoresdeaire")}
+          >
+            Compresores de Aire
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("elevadoresdeauto")}
+          >
+            Elevadores de Autos
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("extractoresycomprimidos")}
+          >
+            Extractores y Comprimidos
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("gomeria")}
+          >
+            Gomería
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("herramientaselectricas")}
+          >
+            Herramientas Eléctricas
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("herramientasespeciales")}
+          >
+            Herramientas Especiales
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("herramientasmanuales")}
+          >
+            Herramientas Manuales
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("herramientasneumaticas")}
+          >
+            Herramientas Neumaticas
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("insertos")}
+          >
+            Insertos
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("inyeccionelectronica")}
+          >
+            Inyección Electrónica
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("juegosdetubos")}
+          >
+            Juegos de Tubos
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("lubricacion")}
+          >
+            Lubricación
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("mobiliarioparatalleres")}
+          >
+            Mobiliario para Talleres
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("prensaehidraulicos")}
+          >
+            Prensas e Hidraulicos
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("puestaapunto")}
+          >
+            Puesta a Punto
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("sacabollos")}
+          >
+            Sacabollos
+          </button>
+          <button
+            className="categories-div"
+            onClick={() => filterResult("soldadoras")}
+          >
+            Soldadoras
+          </button>
+        </div>
       </div>
-    </div>
+      <div className="categories-cards-container">
+        {data.map((values) => {
+          const { id, title, image, category, link } = values;
+          return (
+            <>
+              <div className="card" key={id}>
+                <div className="card-header">Image</div>
+                <div className="card-body">
+                  <h4>{title}</h4>
+                  <button>link</button>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
