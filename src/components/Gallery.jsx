9e205c8e950ab0acdeadb9356f323c2bc7bbd484
luksfay <luksfaydev@gmail.com>
2022-12-15 16:57:10 -0300

    img added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,18 +1,27 @@
 import React from "react";
+import product1 from "../img/productos_1.jpg"
+import product2 from "../img/productos_2.jpg"
+import product3 from "../img/productos_3.jpg"
+import product4 from "../img/productos_4.jpg"
+import product5 from "../img/productos_5.jpg"
+import "../styles/Gallery.css";
 const Gallery = () => {
   return (
     <div className="cards-container">
         <div className="photo">
-            foto
+            <img src={product1} alt="" />
         </div>
         <div className="photo">
-            foto
+            <img src={product2} alt="" />
         </div>
         <div className="photo">
-            foto
+            <img src={product3} alt="" />
         </div>
         <div className="photo">
-            foto
+            <img src={product4} alt="" />
+        </div>
+        <div className="photo">
+            <img src={product5} alt="" />
         </div>
     </div>
   );
--- src/components/Seller.jsx
+++ src/components/Seller.jsx
@@ -11,9 +11,14 @@ const Seller = () => {
         <h2>Fabián Molinari</h2>
         <h3>Vendedor</h3>
         <p>Cuando hayas visto los catálogos. Sentite libre de contactar conmigo para guiarte en tu compra</p>
-        <div className="whatsapp">
-          <img src={img} alt="wsp" className="wsp-img" />
-        </div>
+        <a 
+          href='https://api.whatsapp.com/send?phone=+543412019025&text=Hola!%20Vengo%20de%20la%20p%C3%A1gina.%20Quisiera%20hacer%20una%20consulta%20sobre%20los%20cat%C3%A1logos'
+          target='_blank'
+          rel="noreferrer"
+          className="whatsapp">
+            <img src={img} alt="wsp" className="wsp-img" />
+        </a>
+        
       </div>
     </div>
     </div>
